e774f0417c847cc77f40ae1224707863b6522a58
sara altaee <sara.korea96@gmail.com>
2020-06-09 00:10:01 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let object=array.find(x=>x.result==='W')
+  if(object=== undefined){
+    return object
+  }
+  else
+  return object.year;
+}
